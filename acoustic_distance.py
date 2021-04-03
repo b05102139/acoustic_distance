b66d10118250e6a1cac5b3f176369adc39e2bac1
Ryan Soh-Eun Shim <33049252+b05102139@users.noreply.github.com>
2021-04-04 04:13:38 +0800

    Add files via upload

--- acoustic_distance.py
+++ acoustic_distance.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sklearn import preprocessing
 from scipy.io.wavfile import read
 from python_speech_features import mfcc
 from python_speech_features import delta
@@ -22,13 +23,18 @@ def acoustic_distance(file1, file2):
                       preemph=0.97,
                       numcep = 12,
                       appendEnergy = True)
-  mfcc_feature1 = cmvn(mfcc_feature1, variance_normalization=True)
-  mfcc_feature2 = cmvn(mfcc_feature2, variance_normalization=True)
+  #mfcc_feature1 = preprocessing.scale(mfcc_feature1) # change to cmvn
+  #mfcc_feature2 = preprocessing.scale(mfcc_feature2) # change to cmvn
+  # CMVN BEFORE DELTA OKAY?
+  #mfcc_feature1 = cmvn(mfcc_feature1, variance_normalization=True)
+  #mfcc_feature2 = cmvn(mfcc_feature2, variance_normalization=True)
   deltas1 = delta(mfcc_feature1, 2)
   double_deltas1 = delta(deltas1, 2)
   deltas2 = delta(mfcc_feature2, 2)
   double_deltas2 = delta(deltas2, 2)
   combined1 = np.hstack((mfcc_feature1, deltas1, double_deltas1))
   combined2 = np.hstack((mfcc_feature2, deltas2, double_deltas2))
+  combined1 = cmvn(combined1, variance_normalization=True)
+  combined2 = cmvn(combined2, variance_normalization=True)
   res = dtw(combined1, combined2, window_type="slantedband", window_args={"window_size" : 200}, distance_only=True)
   return res.distance / (combined1.shape[1] + combined2.shape[1])
